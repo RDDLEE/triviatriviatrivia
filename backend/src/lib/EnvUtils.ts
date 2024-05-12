0c61e6325142540038e327dfdaa351d9179f2b91
RDDLEE <105971226+RDDLEE@users.noreply.github.com>
2024-05-12 05:54:03 -0500

    Add root package.json scripts.

--- backend/src/lib/EnvUtils.ts
+++ backend/src/lib/EnvUtils.ts
@@ -5,23 +5,24 @@ dotenv.config();
 export default class EnvUtils {
   private static readonly PORT = process.env["PORT"];
 
-  private static readonly COUNTDOWN_MULTIPLIER = Number(process.env["COUNTDOWN_MULTIPLIER"]);
+  private static readonly COUNTDOWN_MULTIPLIER = process.env["COUNTDOWN_MULTIPLIER"];
 
-  public static readonly checkEnvVars = (): void => {
+  public static readonly getPort = (): string => {
     if (EnvUtils.PORT === undefined) {
       throw "env.PORT not specified.";
     }
-    if (EnvUtils.COUNTDOWN_MULTIPLIER === undefined) {
-      throw "env.COUNTDOWN_MULTIPLIER not specified.";
-    }
-  };
-
-  public static readonly getPort = (): string => {
     return EnvUtils.PORT;
   };
 
   public static readonly getCountdownMultiplier = (): number => {
-    return EnvUtils.COUNTDOWN_MULTIPLIER;
+    if (EnvUtils.COUNTDOWN_MULTIPLIER === undefined) {
+      throw "env.COUNTDOWN_MULTIPLIER not specified.";
+    }
+    const countdownMultiplier = Number(EnvUtils.COUNTDOWN_MULTIPLIER);
+    if (countdownMultiplier <= 0) {
+      throw "countdownMultiplier <= 0.";
+    }
+    return Number(EnvUtils.COUNTDOWN_MULTIPLIER);
   };
 
 }
